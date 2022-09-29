69a43a07bbb3a10ff78577f41a95ec0385a4f36b
idk <hankhill19580@gmail.com>
2022-09-29 01:30:18 -0400

    create volume dir in docker.sh script

--- docker.sh
+++ docker.sh
@@ -6,4 +6,5 @@ git pull --all
 . "${wd}/env.sh"
 docker build -t eyedeekay/about.i2p .
 docker rm -f about.i2p
+mkdir -p "${HOME}/abouti2p"
 docker run -d --net=host --restart=always --user=user --name=about.i2p --volume="${HOME}/abouti2p":/home/user/about.i2p eyedeekay/about.i2p about.i2p --authkey="${AUTH_KEY}"
