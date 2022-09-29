724ba5ac58e94596d1de63ef9982cd177c0cabc5
idk <hankhill19580@gmail.com>
2022-09-29 01:33:14 -0400

    replace whole default client

--- docker.sh
+++ docker.sh
@@ -8,3 +8,4 @@ docker build -t eyedeekay/about.i2p .
 docker rm -f about.i2p
 mkdir -p "${HOME}/abouti2p"
 docker run -d --net=host --restart=always --user=user --name=about.i2p --volume="${HOME}/abouti2p":/home/user/about.i2p eyedeekay/about.i2p about.i2p --authkey="${AUTH_KEY}"
+docker logs -f about.i2p
